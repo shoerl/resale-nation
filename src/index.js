1fbb5fe9a196226789b38bd096ce368ce6af79ec
shoerl <hoerl.s@northeastern.edu>
2021-12-08 17:40:25 -0500

    fix index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
